a2d820ec0564fe342868dd7c9404b5bcac7737b7
LeRon Atkins <61676827+leatkins@users.noreply.github.com>
2020-03-03 07:20:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -132,6 +132,6 @@ app.post('/process-payment', async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 8000)
+app.listen(process.env.PORT || 8000
   () => console.log(`listening on - http://localhost:${port}`)
 );
